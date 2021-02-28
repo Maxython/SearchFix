6f0b3d1b504649ec46eb9edf0fd8fd990a55928f
Max <maksivanchenko83@gmail.com>
2021-02-28 12:57:15 +0300

    Update site

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('index.html', title='Home page')
+    return render_template('index.html', title='Home page', search=True, home=False, help=False)
 
 @app.route('/home')
 def home():
-    return 'is home'
+    return render_template('index.html', title='Home', search=False, home=True, help=False)
 
 @app.route('/help')
 def help():
-    return 'is help'
+    return render_template('index.html', title='Help', search=False, home=False, help=True)
 
 app.run()
