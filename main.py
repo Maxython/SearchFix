09db5c730fb7003d307ac87d9fbba6050d376a7b
Ivan Max <Maxython>
2021-03-26 16:29:12 +0000

    update all

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template
+from random import randint
 
 app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 @app.route('/')
 def home_page():
-    return render_template('index.html', title='Home page', search=True, home=False, help=False)
+    return render_template('search.html', list=list(range(100)))
 
 @app.route('/home')
 def home():
-    return render_template('index.html', title='Home', search=False, home=True, help=False)
+    return render_template('home.html')
 
 @app.route('/help')
 def help():
-    return render_template('index.html', title='Help', search=False, home=False, help=True)
+    return render_template('help.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.errorhandler(Exception)
+def error(error):
+    return render_template('error.html', url=f'/static/img/siba{randint(1, 4) if randint(1, 10) == 1 else randint(1, 3)}.jpg')
 
 app.run()
 
--- static/script.js
+++ static/script.js
@@ -1,1 +1,2 @@
-
+//let menu = document.getElementById("menu");
+//$('main').css('padding', '${height} 20px 0')
